7c7d9ebe6cc8426e479e3be96f710bbb492bb26c
頼矢 <braiya.asuket@icloud.com>
2021-04-19 11:13:23 +0000

    １４章投稿データ

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -1,20 +1,31 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
 use App\News;
-
 class NewsController extends Controller
 {
-    public function add() {
-         return view('admin.news.create');
-    }
-     public function create(Request $request)
+  public function add()
   {
-      // admin/news/createにリダイレクトする
+      return view('admin.news.create');
+  }
+  public function create(Request $request)
+  {
+      $this->validate($request, News::$rules);
+      $news = new News;
+      $form = $request->all();
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+      } else {
+          $news->image_path = null;
+      }
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+      // データベースに保存する
+      $news->fill($form);
+      $news->save();
       return redirect('admin/news/create');
-  }  
+  }
 }
